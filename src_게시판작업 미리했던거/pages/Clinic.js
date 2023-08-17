acb7df99ee1507ca9db567c4422131fa5d060d28
Hyun <sh941217@naver.com>
2023-08-17 12:01:51 +0900

    first

--- src_게시판작업 미리했던거/pages/Clinic.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Clinic() {
-  return (
-    <div>Clinic</div>
-  )
-}
--- src_백업230816/pages/mobile/MobileHome.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function MobileHome() {
-  return (
-    <div>MobileHome</div>
-  )
-}
