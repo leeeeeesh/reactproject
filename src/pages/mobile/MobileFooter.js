3d1d969060487f47f45f4866e142ac69b13e9b92
Hyun <sh941217@naver.com>
2023-08-18 16:01:03 +0900

    add m footer

--- src/pages/mobile/MobileFooter.js
+++ src/pages/mobile/MobileFooter.js
@@ -4,7 +4,31 @@ import styles from './mobilefooter.module.css'
 export default function MobileFooter() {
   return (
     <div id={styles.mobile_footer_wrap}>
-      <footer>MobileFooter</footer>
+      <footer>
+        <h2 className='hidden'>하단영역</h2>
+
+        <div id={styles.mobile_footer_sns}>
+          <h3 className='hidden'>SNS</h3>
+          <img src='./images/logo.png' alt='로고이미지'/>
+
+          <ul id={styles.mobile_sns_list}>
+            <li><i className="fa-brands fa-instagram"></i></li>
+            <li><i className="fa-brands fa-square-facebook"></i></li>
+            <li><i className="fa-brands fa-twitter"></i></li>
+          </ul>
+        </div>
+
+        <div id={styles.mobile_footer_contact}>
+          <h3>Contact Us</h3>
+          
+          <ul>
+            <li>ddbdochi@dochi.co.kr</li>
+            <li>#129, Okhyeon-ro, Nam-gu, Ulsan Metrop olitan City, 
+                802, Ulsan Venture Building</li>
+            <li>1-800-123-1234</li>
+          </ul>
+        </div>
+      </footer>
     </div>
   )
 }
