560e015dabac0251d8b54dd84fbc2ef07bd6ac3d
Hyun <sh941217@naver.com>
2023-08-18 12:07:13 +0900

    add home_01

--- src/pages/mobile/MobileHome.js
+++ src/pages/mobile/MobileHome.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import MobileClinic from '../../components/mobile/mobileclinic/MobileClinic'
+import MobileInfo from '../../components/mobile/mobileinfo/MobileInfo'
+import MobileVisual from '../../components/mobile/mobilevisual/MobileVisual'
 
 export default function MobileHome() {
   return (
-    <div>MobileHome</div>
+    <div>
+      <MobileVisual/>
+      <MobileInfo/>
+      <div></div>
+      <MobileClinic/>
+    </div>
   )
 }
