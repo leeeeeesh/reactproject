04aed69f11b603e14fe1cccd0f5a400a1e5e82ef
Hyun <sh941217@naver.com>
2023-08-18 14:11:21 +0900

    test_02

--- src/components/mobile/mobileclinic/MobileClinic.js
+++ src/components/mobile/mobileclinic/MobileClinic.js
@@ -5,14 +5,16 @@ export default function MobileClinic() {
   return (
     <div id={styles.mobileclinic_wrap}>
       <section id={styles.mobileclinic}>
-        <h2 className={styles.mobileclinic_titie}>VET Clinic</h2>
+        <h2 className={styles.mobileclinic_titie}>VET CLINIC</h2>
         <p className={styles.mobileclinic_text}>검색창에서 고슴도치 진료가 가능한 병원을 빠르게 검색해 보세요.</p>
+
         <form>
-          <div>
+          <div className={styles.mobileclinic_search_wrap}>
             <input type='text'/>
             <button><i className="fa-solid fa-magnifying-glass"></i></button>
           </div>
         </form>
+        
       </section>
     </div>
   )
